0cebe4da43213b5d77f8c9d997c09534c6f1bbf3
Narasimha1997 <yogaimpression@gmail.com>
2017-08-24 13:22:17 +0530

    alpha

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, send_from_directory
+from gtts import gTTS
+app=Flask(__name__)
+
+def sound_gen(text,lan,mode):
+    tts=gTTS(text=text,slow =mode, lang = lan )
+    tts.save("cache/text.mp3")
+
+@app.route('/home',methods=['GET'])
+def home():
+    return render_template('home.html')
+
+@app.route('/start',methods=['GET'])
+def start():
+    return render_template('main.html')
+
+@app.route('/convert', methods=['POST'])
+def convert():
+    text=request.form['txt']
+    lan=request.form['lan']
+    res=False
+    if request.form['mode']=='slow':
+        res=True
+    sound_gen(text=text,lan=lan,mode=res)
+    return send_from_directory(directory='cache', filename='text.mp3')
